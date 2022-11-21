1c92ef022b213d60a3cb991d66fa96eec52f7c58
knapptr <knapptr@gmail.com>
2022-11-21 09:43:02 -0500

    cleans up init

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -2,21 +2,22 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 
 local function assignDefaultKeybinds(server)
   server.setup({
-  capabilities = capabilities,
-  on_attach = function()
-    vim.keymap.set("n","K", vim.lsp.buf.hover,{buffer=0})
-    vim.keymap.set("n","gd", vim.lsp.buf.definition,{buffer=0})
-    vim.keymap.set("n","gt", vim.lsp.buf.type_definition,{buffer=0})
-  end
+    capabilities = capabilities,
+    on_attach = function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+    end
   })
 end
 
 local servers = {
   require "lsp.ts",
   require "lsp.bash",
-  require "lsp.lua"
+  require "lsp.lua",
+  require "lsp.c"
 }
 
-for _,server in ipairs(servers) do
+for _, server in ipairs(servers) do
   assignDefaultKeybinds(server)
 end
