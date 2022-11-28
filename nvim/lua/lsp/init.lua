913164698a8c032d2becd6f0a03f4e467838b3d2
knapptr <knapptr@gmail.com>
2022-11-28 10:33:32 -0500

    adds rust lsp

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,23 +1,24 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local function assignDefaultKeybinds(server)
-  server.setup({
-    capabilities = capabilities,
-    on_attach = function()
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-    end
-  })
+    server.setup({
+        capabilities = capabilities,
+        on_attach = function()
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+        end
+    })
 end
 
 local servers = {
-  require "lsp.ts",
-  require "lsp.bash",
-  require "lsp.lua",
-  require "lsp.c"
+    require "lsp.ts",
+    require "lsp.bash",
+    require "lsp.lua",
+    require "lsp.c",
+    require "lsp.rust"
 }
 
 for _, server in ipairs(servers) do
-  assignDefaultKeybinds(server)
+    assignDefaultKeybinds(server)
 end
